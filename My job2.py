654e5f29c267b79ccf3bfc39e6199d0612137110
Iurii62Rus <kharlamov.yury2017@yandex.ru>
2024-07-24 20:51:22 +0300

    Add files via upload

---
+++ My job2.py
@@ -0,0 +1,14 @@
+completed_tasks = 12
+hours_spent = 1.5
+course_name = 'Python'
+average_time_per_task = hours_spent / completed_tasks
+print('Курс:', course_name, ', всего задач:', completed_tasks, ', затрачено часов:', hours_spent, ', среднее время выполнения', average_time_per_task, 'часа.')
+
+
+
+
+
+
+
+
+
